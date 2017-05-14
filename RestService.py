4d208cd1f93ec44b900a9eec01aeac6d20a19bd3
ItallMakesense <kirgenvall@gmail.com>
2017-05-14 20:46:09 +0300

    DELETE method added, id getter corrected (due to it), args support added (ip and host)

--- RestService.py
+++ RestService.py
@@ -2,12 +2,18 @@
 
 """
 HTTP REST service for performing shell commands on a Linux host.
+Written for python v.2 usage.
 
 Features:
 - Uses only python standard library
+- Supports REST methods: GET, POST, DELETE*
 - Returns appropriate HTTP codes for responses
 - At the same time can be executed a few commands
 - Respects Cyrillic alphabet for requests and responses
+- Accepts user specified address
+  (use "python RestService.py -h" for instructions)
+
+* - DELETE method works on "jobs" and "results" entries
 
 Examples:
 $: curl -i http://localhost:5000/api/jobs
@@ -34,6 +40,12 @@ $: curl -i http://localhost:5000/api/results
 
 $: curl -i http://localhost:5000/api/results/1
 (Client recieves "OK" response and json-formatted job result)
+
+$: curl -i -X DELETE http://localhost:5000/api/results/1
+(Client recieves "OK" response after successful deletion of a result from list)
+
+$: curl -i -X DELETE http://localhost:5000/api/jobs/1
+(Client recieves "OK" response after successful deletion of a job from list)
 """
 
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
@@ -41,9 +53,9 @@ from threading import Thread
 import sys
 import re
 import json
-import cgi
 import datetime
 import subprocess
+import argparse
 
 
 # Changing encoding to support
@@ -62,6 +74,19 @@ class Service(object):
     statuses = {}
     results = {}
 
+    job_id = 0
+    result_id = 0
+
+    @classmethod
+    def get_job_id(self):
+        self.job_id += 1
+        return str(self.job_id)
+
+    @classmethod
+    def get_result_id(self):
+        self.result_id += 1
+        return str(self.result_id)
+
 
 class HTTPRequestHandler(BaseHTTPRequestHandler):
 
@@ -72,51 +97,37 @@ class HTTPRequestHandler(BaseHTTPRequestHandler):
         r'/api/results$': ("results", Service.results.values())
         }
         self.branches = {
-        r'/api/jobs/[^\D]+': ("job", Service.jobs, self.send_job_info),
+        r'/api/jobs/[^\D]+': ("status id", Service.jobs, self.send_job_info),
         r'/api/statuses/[^\D]+':("status", Service.statuses, self.send_status),
-        r'/api/results/[^\D]+': ("result", Service.results, self.send_result)
+        r'/api/results/[^\D]+': ("result id", Service.results, self.send_result)
         }
         return BaseHTTPRequestHandler.__init__(self, *args, **kwargs)
-    
-    def spec_response(self, code):
-        if code == 200:
-            self.send_response(200)
-            self.send_header("Content-Type", "application/json")
-            self.end_headers()
-        elif code == 404:
-            self.send_response(404, "Not Found: job id don\'t exist")
-            self.end_headers()
-        elif code == 400:
-            self.send_response(400, "Bad Request: enter address properly")
-            self.end_headers()
 
 ###################### METHOD - GET ######################
     def do_GET(self):
         send = False
-        while not send:
 # Searching matches with existing major endpoints
-            for rgx, res in self.tree.items():
-                if re.search(rgx, self.path):
-                    self.send_response(200)
-                    self.send_header("Content-Type", "application/json")
-                    self.end_headers()
-                    self.wfile.write(json.dumps({res[0]: res[1]},\
-                                                indent=2) + "\n")
-                    send = True
-# Searching matches with existing id-specified endpoints
-            for rgx, res in self.branches.items():
-                if re.search(rgx, self.path):
-                    branch_id = self.path.split('/')[-1]
-                    self.branch_response(res, branch_id)
-                    send = True
-# No matces with existing endpoints - bad request
-            if not send:
-                self.send_response(400, "Bad Request: enter address properly")
+        for rgx, res in self.tree.iteritems():
+            if re.search(rgx, self.path):
+                self.send_response(200)
+                self.send_header("Content-Type", "application/json")
                 self.end_headers()
-                break
+                self.wfile.write(json.dumps({res[0]: res[1]},\
+                                            indent=2) + "\n")
+                send = True
+# Searching matches with existing id-specified endpoints
+        for rgx, res in self.branches.iteritems():
+            if re.search(rgx, self.path):
+                branch_id = self.path.split('/')[-1]
+                self.branch_response(res, branch_id)
+                send = True
+# No matces with existing endpoints - bad request
+        if not send:
+            self.send_response(400, "Bad Request: enter address properly")
+            self.end_headers()
 
     def branch_response(self, branch, branch_id):
-        if branch_id in branch[1].keys():
+        if branch_id in branch[1].iterkeys():
 # Sending header of request
             branch[2](branch_id)
             self.end_headers()
@@ -124,7 +135,7 @@ class HTTPRequestHandler(BaseHTTPRequestHandler):
             self.wfile.write(json.dumps(branch[1][branch_id],\
                                         indent=2) + "\n")
         else:
-            self.send_response(404, "Not Found: job id don\'t exist")
+            self.send_response(404, "Not Found: job with such id don\'t exist")
             self.end_headers()
 
     def send_job_info(self, job_id): ### Sends job endpoint header
@@ -135,26 +146,24 @@ class HTTPRequestHandler(BaseHTTPRequestHandler):
 
     def send_status(self, job_id): ### Sends status endpoint header
 # If job is done, includes redirection link
-            if Service.statuses[job_id]["done"]:
-                result_id = Service.statuses[job_id]["result id"]
-                self.send_response(303, "See Other: execution completed")
-                self.send_header("Content-Type", "application/json")
-                self.send_header("Locaion", "{}/api/results/{}".format(\
-                                            Service.address, result_id))
-            else:
-                self.send_response(200)
-                self.send_header("Content-Type", "application/json")
-
-    def send_result(self, result_id): ### Sends result endpoint header
+        if Service.statuses[job_id]["status"] in ("executing", "deleted"):
             self.send_response(200)
             self.send_header("Content-Type", "application/json")
+        elif Service.statuses[job_id]["status"] == "done":
+            result_id = Service.statuses[job_id]["result id"]
+            self.send_response(303, "See Other: execution had completed")
+            self.send_header("Content-Type", "application/json")
+            self.send_header("Locaion", "{}/api/results/{}".format(\
+                                        Service.address, result_id))
+
+    def send_result(self, result_id): ### Sends result endpoint header
+        self.send_response(200)
+        self.send_header("Content-Type", "application/json")
 
 ###################### METHOD - POST ######################
     def do_POST(self):
-###CREATE### Retrieving information about a new job, and creating it
         if re.search(r'/api/jobs$', self.path):
-            ctype, pdict = cgi.parse_header(self.headers.getheader("content-type"))
-            if ctype == "application/json":
+            if self.headers.getheader("content-type") == "application/json":
 # Getting job content
                 length = int(self.headers.getheader("content-length"))
                 content = json.loads(self.rfile.read(length))
@@ -165,8 +174,9 @@ class HTTPRequestHandler(BaseHTTPRequestHandler):
 # Sending resonse header
                 self.send_response(202)
                 self.send_header("Locaion", "{}/api/statuses/{}".format(\
-                    Service.address, job_id))
+                                    Service.address, job_id))
                 self.end_headers()
+
 # Executing new job
                 self.execute_in_thread(job_id, time)
             else:
@@ -179,9 +189,9 @@ class HTTPRequestHandler(BaseHTTPRequestHandler):
     def create_job(self, content): ### Creates a new job and executes it in separate thread
         creation_time = datetime.datetime.now()
         creation_day = datetime.date.today().strftime("%A")
-        job_id = str(len(Service.jobs) + 1)
+        job_id = Service.get_job_id()
         Service.jobs.setdefault(job_id, {
-            "job_id": job_id,
+            "job id": job_id,
             "command": content["command"],
             "created": str(creation_time).replace(" ", creation_day[0])
             })
@@ -189,8 +199,8 @@ class HTTPRequestHandler(BaseHTTPRequestHandler):
 
     def add_status(self, job_id): ### Adds the job to the status list
         Service.statuses.setdefault(job_id, {
-            "status_id": job_id,
-            "done": False
+            "status id": job_id,
+            "status": "executing"
             })
 
     def execute_in_thread(self, job_id, time): ### Executes the job in a new thread
@@ -206,13 +216,13 @@ class HTTPRequestHandler(BaseHTTPRequestHandler):
 # Writing a result of job execution
         result_id = self.add_result(stdout, stderr, ex_code, time)
 # Updating jobs status list
-        Service.statuses[job_id]["done"] = True
+        Service.statuses[job_id]["status"] = "done"
         Service.statuses[job_id].setdefault("result id", result_id)
 
-    def add_result(self, out, err, code, time): ### Writes a result of a job execution
+    def add_result(self, out, err, code, time): ### Writes a result of the job execution
         completion_time = datetime.datetime.now()
         completion_day = datetime.date.today().strftime("%A")
-        result_id = str(len(Service.results) + 1)
+        result_id = Service.get_result_id()
 # Adding result to the jobs result list
         Service.results.setdefault(result_id, {
             "result id": result_id,
@@ -222,6 +232,45 @@ class HTTPRequestHandler(BaseHTTPRequestHandler):
             "stderr": err.decode(),
             "exit code": code })
         return result_id
+
+###################### METHOD - DELETE ######################
+    def do_DELETE(self):
+        proper = False
+        for rgx, res in self.branches.iteritems():
+# Secure statuses form deletion
+            if re.search(rgx, self.path) and res[0] == "status":
+                proper = True
+                self.send_response(403)
+                self.end_headers()
+                continue
+# Searching for matches with existing id-specified endpoints
+            elif re.search(rgx, self.path):
+                proper = True
+                branch_id = self.path.split('/')[-1]
+# Deleting entry
+                self.delete_item(res, branch_id)
+# Updating status
+                self.upd_status(res, branch_id)
+        if not proper:
+            self.send_response(400, "Bad Request: enter address properly")
+            self.end_headers()
+
+    def delete_item(self, branch, branch_id):
+        deleted = branch[1].pop(branch_id, None)
+        if deleted:
+            self.send_response(200, "OK: entry deleted")
+            self.send_header("Content-Type", "application/json")
+            self.end_headers()
+        else:
+            self.send_response(404, "Not Found: job with such id don\'t exist")
+            self.end_headers()
+
+    def upd_status(self, branch, branch_id):
+        for job_id, status in Service.statuses.iteritems():
+            if status[branch[0]] == branch_id:
+                Service.statuses[job_id]["status"] = "deleted"
+                return
+
 
 class Server(object):
 
@@ -241,5 +290,11 @@ class Server(object):
             self.server.server_close()
  
 if __name__=="__main__":
-    server = Server(Service.ip, Service.port)
+    parser = argparse.ArgumentParser(description='HTTP REST Server')
+    parser.add_argument('ip', default=Service.ip, nargs='?',\
+                        help='HTTP Server IP (127.0.0.1 by default)')
+    parser.add_argument('port',type=int, default=Service.port, nargs='?',\
+                        help='Port for HTTP Server (5000 by default)')
+    args = parser.parse_args()
+    server = Server(args.ip, args.port)
     server.start()
