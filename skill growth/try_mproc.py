baada89d4f8b12349193b3cc05bc3c35547e0e7f
ItallMakesense <kirgenvall@gmail.com>
2017-05-17 21:06:57 +0300

    Mess with mproc

--- skill growth/try_mproc.py
+++ skill growth/try_mproc.py
@@ -1,43 +1,54 @@
 from collections import Counter
+import multiprocessing
 import os
-import re
 
 
-def is_among(name, patterns):
-    for pattern in (re.escape(_) for _ in patterns):
-        if re.search(pattern.replace('\*', '.*'), name):
-            return True
-    return False
+occurs = Counter()
 
-def match(entry, patterns):
-    for p in patterns:
-        if p.startswith('*') and entry.endswith(p.lstrip('*')):
-            return True
-        elif p.endswith('*') and entry.startswith(p.rstrip('*')):
-            return True
-        elif p == entry:
-            return True
-    return False
+def among(name, patterns):
+    """ Makes sense of constructions like *his or thi*.
+    """
+    match = False
+    for pattern in patterns:
+        if pattern == '*':
+            match = name == pattern
+        elif pattern.startswith('*'):
+            match = name.endswith(pattern[1:])
+        elif pattern.endswith('*'):
+            match = name.startswith(pattern[:-1])
+        elif name == pattern:
+            match = True
+        if match:
+            return match
+    return match
+
+def multi_counter(file, ignored, min_len):
+    with open(file) as o_file:
+        try:
+            content = (word for word in o_file.read().split()\
+                        if not among(word, ignored)\
+                        and len(word) >= min_len)
+            occurs.update(Counter(content))
+        except UnicodeError as error:
+            print("{} - Unsupported text file. Error message:\n{}".format(\
+                    file, error))
 
 def words_counter(path, glob_patterns, ignored_words, min_word_len):
     files_paths = []
-    words = []
-    occurs = {}
-    with os.scandir(path) as full_dir:
-        for entry in full_dir:
-            if entry.is_file() and is_among(entry.name, glob_patterns):
-                files_paths.append(entry.path)
-    print(files_paths)
+    for entry in os.scandir(path):
+        if entry.is_file() and among(entry.name, glob_patterns):
+            files_paths.append(entry.path)
     for file in files_paths:
-        with open(file) as o_file:
-            content = (word for word in o_file.read().split()\
-                        if not is_among(word, ignored_words) and len(word) >= min_word_len)
-    return Counter(content)
+        # mproc = multiprocessing.Process(target=multi_counter,\
+        #         args=(file, ignored_words, min_word_len))
+        # mproc.start()
+        multi_counter(file, ignored_words, min_word_len)
+    return dict(occurs)
 
 words = words_counter(
-    path='/var/log',
-    glob_patterns=('*log', '*.log'),
-    ignored_words=('May', 'ret*'),
+    path='/usr/bin',
+    glob_patterns=('py*', ),
+    ignored_words=('import', 'from', 'def*', 'con*', '*if'),
     min_word_len=3
     )
 print(words)
