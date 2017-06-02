239fae8bde9be3fa0c098adf0525b7fec8ae235d
ItallMakesense <kirgenvall@gmail.com>
2017-06-02 20:51:47 +0300

    Done (no comments, will write soon)

--- skill growth/descr_3.py
+++ skill growth/descr_3.py
@@ -4,10 +4,9 @@ from datetime import datetime
 
 class Person(Model):
 
-    def __init__(self): ### Надо ПЕРЕДЕЛАТЬ - если делать через инит, то дальше обьекты-дискрипторы будут перезаписаны обычной строкой
-        self.name = NameField()
-        self.birthday = BirthdayField()
-        self.phone = PhoneField()
+    name = NameField()
+    birthday = BirthdayField()
+    phone = PhoneField()
 
 
 if __name__ == "__main__":
@@ -15,8 +14,3 @@ if __name__ == "__main__":
     p.name = "Me"
     p.phone = "133 13 1331313"
     p.save(prim_key='name') # All changes saved
-    p.birthday = datetime.strptime("1011-12-13", "%Y-%m-%d")
-    p.save(prim_key='name')
-    p2 = Person()
-    p2.name = "Else"
-    p2.save(prim_key='name')
--- skill growth/myorm.py
+++ skill growth/myorm.py
@@ -9,58 +9,27 @@ import re
 
 class Model(object):
 
-    def write(self, namespace, prim_key):
+    def save(self, prim_key):
+        filter_func = lambda attr: True if not attr.startswith('_')\
+                                    and not callable(getattr(self, attr))\
+                                    else False
+        namespace = list(filter(filter_func, dir(self)))
         table_name = self.__class__.__name__
-        # column_types = ",".join(["%s text" % name for name in namespace])
-        column_types = []
-        column_values = []
-        columns = []
-        for name, value in namespace.items():
-            column_values.append(str(value))
-            columns.append(name)
-            if name == prim_key:
-                column_types.append("%s text primary key" % name)
-                continue
-            column_types.append("%s text" % name)
+        column_names = ",".join(namespace)
+        column_values = [str(getattr(self, attr)) for attr in namespace]
+        column_types = ["%s text" % name for name in namespace]
+        column_types = ["%s primary key" % col if prim_key in col\
+                        else col for col in column_types]
         column_types = ",".join(column_types)
-        column_names = ",".join(columns)
-        print(columns)
-        print(column_values)
+
         db = sqlite3.connect("orm.db")
         gate = db.cursor()
+
         gate.execute("create table if not exists {} ({})".format(table_name, column_types))
-        update_values = ",".join(["%s=\"%s\"" % pair for pair in zip(columns, column_values)])
         gate.execute("insert or replace into {} ({}) values (?,?,?)".format(\
                         table_name, column_names), column_values)
         db.commit()
-        # print([line for line in gate.execute("select * from %s" % table_name)])
         db.close()
-
-    def save(self, prim_key):
-        # filter_func = lambda attr: True if not attr.startswith('_') and\
-        #                            not callable(getattr(self, attr))\
-        #                            else False
-        # namespace = list(filter(filter_func, self.__dict__))
-        self.write(self.__dict__, prim_key)
-
-    # def __getattribute__(self, name):
-    #     if name.startswith("__") or name == "save":
-    #         return super().__getattribute__(name)
-    #     else:
-    #         obj = self
-    #         obj_type = type(self)
-    #         self = obj.__dict__[name]
-    #         print(obj, obj_type, self)
-    #         return self.__get__(obj, obj_type)
-
-    def __getattribute__(self, key):
-        v = object.__getattribute__(self, key)
-        if hasattr(v, '__get__'):
-            return v.__get__(self, None, type(self))
-        return v
-
-    def __get__(self, obj, obj_type):
-        print(self, obj, obj_type)
 
 
 class NameField(object):
